1a25a8819d425ba105deca3edab9ae07f847c751
jeeyoung1301 <jeeyoung1301@naver.com>
2021-09-13 21:05:16 +0900

    company

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-    $('.slider').bxSlider();
+    const slider1 = $('.slider').bxSlider({
+        controls:false,
+    });
+    $('.nextBtn').click(function(){
+        slider1.goToNextSlide();
+    });
+    $('.prevBtn').click(function(){
+        slider1.goToPrevSlide();
+    });
+
+    // $('.toggleMenu').click();
+    // $('.toggleMenu').on('이벤트명',리스너);
+    // $('.toggleMenu').on('click',function(){});
+
+
+    $('.toggleMenu').click(function(){
+        const toggleMenu = $('.toggleMenu').attr('class'); // value toggleMenu
+        console.log(toggleMenu);
+        if(toggleMenu == 'toggleMenu'){
+          //  $('.toggleMenu').addClass('active'); //제이쿼리
+          document.getElementsByClassName('toggleMenu')[0].classList.add('active'); //자바스크립트
+          $('.gnb').addClass('view');
+        }
+        else{
+            // $('.toggleMenu').removeClass('active');
+            document.getElementsByClassName('toggleMenu')[0].classList.remove('active'); //자바스크립트
+            $('.gnb').removeClass('view');
+        }
+    });
+
+    $(window).resize(function(){
+        $('.toggleMenu').removeClass('active');
+        $('.gnb').removeClass('view');
+    });
 });
