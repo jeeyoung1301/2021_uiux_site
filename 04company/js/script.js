81f71c0818d9f8f40bf5e8434f7d52ece91ec310
jeeyoung1301 <jeeyoung1301@naver.com>
2021-10-06 08:12:26 +0900

    company

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     const visual_01 = $('.slider').bxSlider({
         controls:false,
-        auto:true,
     });
     $('.nextBtn').click(function(){
         visual_01.goToNextSlide();
