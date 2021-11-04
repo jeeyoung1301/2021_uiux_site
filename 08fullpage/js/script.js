500a0d3bbe59ad5c3abc04ee1b11fefe1d47c11b
jeeyoung1301 <jeeyoung1301@naver.com>
2021-11-05 01:59:39 +0900

    fullpage

---
+++ 08fullpage/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#fullpage').fullpage({
+        autoScrolling:true,
+        scrollHorizontally:true,
+        navigation:true,
+        anchors:['section1','section2','section3','section4','section5'],
+    })
+})
