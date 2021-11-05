8bae2857df9901542f8cb102338f66971a23861a
jeeyoung1301 <jeeyoung1301@naver.com>
2021-11-05 18:19:24 +0900

    fullpage

--- 08fullpage/js/script.js
+++ 08fullpage/js/script.js
@@ -4,5 +4,6 @@ $(function(){
         scrollHorizontally:true,
         navigation:true,
         anchors:['section1','section2','section3','section4','section5'],
+        sectionsColor:['#ccc','#bbb','pink','skyblue','ivory'],
     })
 })
