da7a3482865ee8e2693284059abf5535864a756a
jeeyoung1301 <jeeyoung1301@naver.com>
2021-11-15 04:15:36 +0900

    fullpage

--- 08fullpage/js/script.js
+++ 08fullpage/js/script.js
@@ -4,6 +4,7 @@ $(function(){
         scrollHorizontally:true,
         navigation:true,
         anchors:['section1','section2','section3','section4','section5'],
-        sectionsColor:['#ccc','#FFF9E9','#FACAC6','skyblue','#104138'],
+        sectionsColor:['#ccc','#FFF9E9','#FACAC6','#F1F1F1','#104138'],
+        navigationTooltips:['Hello!','innisfree X Knotted','노세범 파우더 쿠션','']
     });
 });
