634794330575c58d98290b2778427fb9565d337e
jeeyoung1301 <jeeyoung1301@naver.com>
2022-02-08 18:59:39 +0900

    fullpage

--- 08fullpage/js/script.js
+++ 08fullpage/js/script.js
@@ -4,7 +4,7 @@ $(function(){
         scrollHorizontally:true,
         navigation:true,
         anchors:['section1','section2','section3','section4','section5'],
-        sectionsColor:['#ccc','#FFF9E9','#FACAC6','#F1F1F1','#104138'],
+        sectionsColor:['#ccc','#FFF9E9','#FACAC6','#FFF9E9','#104138'],
         navigationTooltips:['Hello!','innisfree X Knotted','노세범 파우더 쿠션','관심제품&리뷰','Info']
     });
 });
