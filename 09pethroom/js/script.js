1aadb6e57e533fb0502493bb332d56838be2302b
jeeyoung1301 <jeeyoung1301@naver.com>
2022-02-20 06:55:16 +0900

    pethroom

--- 09pethroom/js/script.js
+++ 09pethroom/js/script.js
@@ -1,12 +1,6 @@
-const searchIcon = document.querySelector('.search_icon');
-const searchModal = document.querySelector('.search_modal')
-const searchClose = document.querySelector('.search_close')
-
-searchModal.addEventListener('click',function(e){
-    if(e.target.classList.contains('searchModal')){
-        searchModal.style.display = 'none';
-    }
-});
+const searchIcon = document.querySelector('.search_icon'),
+      searchModal = document.querySelector('.search_modal'),
+      searchClose = document.querySelector('.search_close');
 
 searchIcon.addEventListener('click',function(){
     searchModal.style.display = 'block';
@@ -14,4 +8,26 @@ searchIcon.addEventListener('click',function(){
 
 searchClose.addEventListener('click',function(){
     searchModal.style.display = 'none';
-})
+});
+
+const sliderWrapper01 = document.querySelector('.visual_slide'),
+      sliderContainer01 = document.querySelector('.slider01');
+let  slides01 = document.querySelectorAll('.slider01 li'),
+     slideCount01 = slides01.length,
+     currentIndex01 = 0,
+     topHeight01 = 0;
+// function calTallestSlide(){
+//     for(let i=0;i<slideCount01;i++){
+//       if(slides01[i].offsetHeight > topHeight01){
+//           topHeight01 = slides01[i].offsetHeight;
+//       }
+//     }
+
+//     sliderWrapper01.style.height = `${topHeight01}px`;
+//     sliderContainer01.style.height = `${topHeight01}px`;
+// }
+// calTallestSlide();
+
+for(let i=0;i<slideCount01;i++){
+    slides01[i].style.left = `${i * 100}%`; 
+}
