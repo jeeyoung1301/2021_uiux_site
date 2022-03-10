a47396d2ebc347a9e2723e853cf5bb7725cef9ea
jeeyoung1301 <jeeyoung1301@naver.com>
2022-03-11 04:45:09 +0900

    cj

---
+++ 10dior/script.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector("header");
+// let mainMenu = document.querySelectorAll(".menu > li");
+// let submenuWrap = document.querySelectorAll(".submenuWrap");
+
+// for (let i = 0; i < mainMenu.length; i++) {
+// 	mainMenu[i].addEventListener("mouseover", (e) => {
+// 		header.style.height = "160px";
+// 		e.currentTarget.submenuWrap.style.display = "block";
+// 	});
+// 	mainMenu[i].addEventListener("mouseleave", (e) => {
+// 		header.style.height = "80px";
+// 	});
+// }
