baafb7114caf70207c0eee6a8174412984dbfdd0
jeeyoung1301 <jeeyoung1301@naver.com>
2022-03-19 03:04:19 +0900

    cj

--- 10cjenmenter/script.js
+++ 10cjenmenter/script.js
@@ -11,6 +11,29 @@
 // 		header.style.height = "80px";
 // 	});
 // }
+const swiper01 = new Swiper(".swiper01", {
+	speed: 500,
+	loop: true,
+	direction: "horizontal",
+	centeredSlides: true,
+	autoplay: 1000,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
+const swiper02 = new Swiper(".swiper02", {
+	speed: 1000,
+	loop: true,
+	direction: "horizontal",
+	centeredSlides: true,
+	slidesPerView: 4,
+	spaceBetween: 5,
+	navigation: {
+		prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next",
+	},
+});
 
 const familySite = document.querySelector(".familySite > p");
 const familyList = document.querySelector(".familySiteList");
