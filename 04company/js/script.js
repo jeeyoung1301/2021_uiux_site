a5bd1ee577d4c0a3c8f99b5ea7cb9b5c9b2b7f42
jeeyoung1301 <jeeyoung1301@naver.com>
2022-04-15 22:24:49 +0900

    cj

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,70 +1,33 @@
-const header = document.getElementById('headerWrap')
-document.body.addEventListener('scroll',function(){
-    let y = document.body.scrollTop;
+const header = document.getElementById("headerWrap");
+document.body.addEventListener("scroll", function () {
+	let y = document.body.scrollTop;
 
-    if(y> 60){
-        header.classList.add('active')               
-    }
-    else{
-        header.classList.remove('active')
-    }
+	if (y > 60) {
+		header.classList.add("active");
+	} else {
+		header.classList.remove("active");
+	}
 });
 
+$(document).ready(function () {
+	$(".toggleMenu").click(function () {
+		const toggleMenu = $(".toggleMenu").attr("class");
+		console.log(toggleMenu);
+		if (toggleMenu == "toggleMenu") {
+			document
+				.getElementsByClassName("toggleMenu")[0]
+				.classList.add("active");
+			$(".gnb").addClass("view");
+		} else {
+			document
+				.getElementsByClassName("toggleMenu")[0]
+				.classList.remove("active");
+			$(".gnb").removeClass("view");
+		}
+	});
 
-$(document).ready(function(){
-    const visual_01 = $('.slider').bxSlider({
-        controls:false,
-    });
-    $('.nextBtn').click(function(){
-        visual_01.goToNextSlide();
-    });
-    $('.prevBtn').click(function(){
-        visual_01.goToPrevSlide();
-    });
-
-    $('.slider_01').bxSlider({
-        controls:false,
-    })
-
-    $('.slider_02').bxSlider({
-        controls:false,
-    });
-    $('.slider_03').bxSlider({
-        controls:false
-    })
-    $('.slider_04').bxSlider({
-        controls:false
-    });
-    $('.slider_04 > li a').colorbox({
-        iframe:true,
-        innerWidth:960,
-        innerHeight:540,
-        maxWidth: '90%',
-        maxHeight: '50%'
-    });
-
-
-
-    // $('.toggleMenu').click(function(){
-    //     const toggleMenu = $('.toggleMenu').attr('class');
-    //     console.log(toggleMenu);
-    //     if(toggleMenu == 'toggleMenu'){
-    //       document.getElementsByClassName('toggleMenu')[0].classList.add('active'); 
-    //       $('.gnb').addClass('view');
-    //     }
-    //     else{
-    //         document.getElementsByClassName('toggleMenu')[0].classList.remove('active');
-    //         $('.gnb').removeClass('view');
-    //     }
-    // });
-
-    // $(window).resize(function(){
-    //     $('.toggleMenu').removeClass('active');
-    //     $('.gnb').removeClass('view');
-
-    // });
-
-
+	$(window).resize(function () {
+		$(".toggleMenu").removeClass("active");
+		$(".gnb").removeClass("view");
+	});
 });
-
-
--- 10cjenmenter/js/script.js
+++ 10cjenmenter/js/script.js
@@ -73,9 +73,9 @@ const slide01 = new Swiper(".slide01", {
 		prevEl: ".swiper-button-prev-slide01",
 		nextEl: ".swiper-button-next-slide01",
 	},
-	// autoplay: {
-	// 	delay: 8000,
-	// },
+	autoplay: {
+		delay: 8000,
+	},
 	breakpoints: {
 		768: {
 			slidesPerView: 2,
